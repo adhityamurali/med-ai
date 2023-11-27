e5955657fc97474dd8f2e38d7cd8bd24384b3af0
adhityamurali <adhityamurali03@gmail.com>
2023-11-28 00:06:11 +0530

    base setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className= 'flex-col items-center justify-center place-items-center mt-[10rem]'>
+      
+      <h1 className="text-5xl text-center font-extrabold font-mono p-4">Hello Med World!!</h1>
+      <p className="text-2xl text-center">stay tuned to get access to your AI companion soon..</p>
+    </div>
+  )
+}
